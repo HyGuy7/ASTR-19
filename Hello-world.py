18ef6fdc5c8c74e1ad381f3523751d5487d4099f
HyGuy7 <112208395+HyGuy7@users.noreply.github.com>
2025-04-02 16:42:02 -0700

    This is the HelloWorld Program

---
+++ Hello-world.py
@@ -0,0 +1,19 @@
+#HelloWorld
+
+def PrintHelloWorld():
+    print("Hello World!")
+
+
+#this defines our main()
+#fuction for the program
+
+def main():
+    PrintHelloWorld()
+
+
+#when we run the program this executes first
+
+if __name__=="__main__":
+    main()
+
+
