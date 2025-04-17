4a775621795eb5d92a239e49bf53ac49a58bffb1
HyGuy7 <112208395+HyGuy7@users.noreply.github.com>
2025-04-17 09:57:27 -0700

    Add files via upload

---
+++ Session-1.py
@@ -0,0 +1,4 @@
+print("Hylen Taylor James")
+print("Pronouns: He/Him")
+print("My favorite movie is called fear and loathing in los vegas, I think it is a wonderful adaptation of the book.")
+print("My favorite food is artichoke, I love it because of the unique umami flavor.")
---
+++ session-2.py
@@ -0,0 +1,19 @@
+
+#float summation
+x = 19.10 #declare a float
+y = 6.9 #float
+print("x+y=",x+y)
+print("type(x+y): ",type(x+y))
+
+
+#integer subtraction
+i = 10 # Declare an integer
+z = 15
+print("z-i=",z-i)
+print("type(z-i): ",type(z-i))
+
+#Product of a float and an integer
+f = 14.50
+r = 6
+print("f*r=",f*r)
+print("type(f*r): ",type(f*r))
